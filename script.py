a649b7f420cd0148fdc2a8452b24ed90782a5bae
Gustavo <gstvrrds@gmail.com>
2024-12-23 15:24:13 -0300

    feat:getting text with selenium

---
+++ script.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import  Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+service = Service("./chromedriver.exe")
+
+def get_driver():
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service,options=options)
+    driver.get("https://www.merxenergia.com.br/")
+    return driver
+def main():
+   driver = get_driver()
+   element = WebDriverWait(driver,10).until(
+       EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/section[1]/section/div[1]/p'))
+   )
+   return element.text
+
+
+print(main())
